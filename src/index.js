356dc249b74a46e321fe329af4ae4d8e620732c5
Jonathan Primeau <jprimeau@spiria.com>
2017-07-05 09:34:18 -0400

    added missing package dependency compression

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import {Provider} from 'react-redux';
 import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
 import './styles/styles.css'; //Webpack can import CSS files too!
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/toastr/build/toastr.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+import 'toastr/build/toastr.min.css';
 
 const store = configureStore();
 
